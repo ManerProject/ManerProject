b478c2957f200b89ec150bd7a060083fabb91988
DawidPlocharski123 <34648643+DawidPlocharski123@users.noreply.github.com>
2018-01-26 14:48:18 +0100

    123
    123

--- IndexN/skrypt_panel_zmiany_ucznia.php
+++ IndexN/skrypt_panel_zmiany_ucznia.php
@@ -1,8 +1,24 @@
 <?php
 session_start();  
-
-
-mysql_query=("update loginy set (`email={$_POST["email"]},haslo={$_POST["haslo"]},klasa={$_POST["klasa"]},login={$_POST["login"]},typ={$_POST["typ"]}`) where typ="uczen"");
-
+require "baza.php";
+$email=$_POST['email'];
+$haslo=$_POST['haslo'];
+$klasa=$_POST['klasa'];
+$login=$_POST['login'];
+$typ=$_POST['typ'];
+if ($result = $wynik->query("update loginy set (`email={$_POST["email"]},haslo={$_POST["haslo"]},klasa={$_POST["klasa"]},login={$_POST["login"]},typ={$_POST["typ"]}`)")) {
+   
+    while($w=$result->fetch_assoc()){
+	
+	}
+	
+}
+	
+	//$result = mysql_query("SELECT * FROM `loginy` WHERE typ='uczen'");
+	
+    //$row = mysql_fetch_array($result);
+	
+//mysql_query=("update loginy set (`email={$_POST["email"]},haslo={$_POST["haslo"]},klasa={$_POST["klasa"]},login={$_POST["login"]},typ={$_POST["typ"]}`)");
+header('Location: panel_zmiany_ucznia.php');
     
 ?>
