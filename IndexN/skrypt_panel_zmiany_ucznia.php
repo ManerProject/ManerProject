6dcfba57c04cbb1e7d2d4205aa59b7381950d61c
DawidPlocharski123 <34648643+DawidPlocharski123@users.noreply.github.com>
2018-02-01 09:33:40 +0100

    Update skrypt_panel_zmiany_ucznia.php

--- IndexN/skrypt_panel_zmiany_ucznia.php
+++ IndexN/skrypt_panel_zmiany_ucznia.php
@@ -1,18 +1,24 @@
 <?php
 session_start();  
 require "baza.php";
-$n=$_SESSION['ilosc'];
-for($i=0;$i<$n;$i++){
-$login=$_POST['login'];
-$haslo=$_POST['haslo'];
-$typ="nieprzydzielony";
-$wynik->query("INSERT INTO `loginy` (`login`,`haslo`,`email`,`typ`,`klasa`) VALUES ('$login[$i]','$haslo[$i]','null','$typ','null')");
-
+$email=$_POST['email[]'];
+$haslo=$_POST['haslo[]'];
+$klasa=$_POST['klasa[]'];
+$login=$_POST['login[]'];
+$typ=$_POST['typ[]'];
+if ($result = $wynik->query("update loginy set (`email=$email,haslo=$haslo,klasa=$klasa,login=$login,typ=$typ`)")) {
+   
+    while($w=$result->fetch_assoc()){
+	
+	}
+	
 }
-
-$wynik->close();
-		
-
-header('Location: panel_dodania_ucznia.php');
-//$wynik->query("INSERT INTO `lekcje` (`lekcja`) VALUES ('$kat')");
+	
+	//$result = mysql_query("SELECT * FROM `loginy` WHERE typ='uczen'");
+	
+    //$row = mysql_fetch_array($result);
+	
+//mysql_query=("update loginy set (`email={$_POST["email"]},haslo={$_POST["haslo"]},klasa={$_POST["klasa"]},login={$_POST["login"]},typ={$_POST["typ"]}`)");
+header('Location: panel_zmiany_ucznia.php');
+    
 ?>
