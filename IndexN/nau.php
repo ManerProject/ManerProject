5c5d2037c19579235ee0f225d9d58ef7798d1b35
KarolPlochar <35262500+KarolPlochar@users.noreply.github.com>
2018-02-01 08:50:22 +0100

    Add files via upload

--- IndexN/nau.php
+++ IndexN/nau.php
@@ -1,62 +1,6 @@
-<?php 
-session_start();
-
-
-
-
-
-
- ?>
-<!DOCTYPE html>
-
-<html>
-
-<head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
-</head>
-<body>
-  <style>
-  body
-  {
-	background-color:#E6E6E6;
-	margin:0px;
-	
-	font-size:35px;
-	font-family: 'Acme', sans-serif;
-	
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
-	width:100%;
-	height:100px;
-	background-color:black;
-	float:right
-	text-align: center;
-	pading:10px;
-	color:white;
-	
-  }
-#okno
-{
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
-}
-  
-  </style>
-
-<div id="all">
-<div id="logo">
+<?php
+require('reqnau.php');
+?>
 SEMESTR1
 <img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
 <form action="../wyloguj.php" method="_POST" >
