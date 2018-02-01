c6e86ed8a4bae8220c6856edb3681aae2e54a0a4
KarolPlochar <35262500+KarolPlochar@users.noreply.github.com>
2018-02-01 09:13:38 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -7,70 +7,10 @@
     <meta name="description" content="Centrum Kształcenia Zawodowego i Ustawicznego w Brodnicy">
     <meta name="keywords" content="ckziu brodnica, technikum, zawodówka">
     <title>CKZiU w Brodnicy</title>
-   <!--<link rel="stylesheet" href="style.css"/>-->
+	<link rel="stylesheet" href="CSS/login.css"/>
   </head>
  
   <body onload="RunAction()">
-  <style>
-  body
-  {
-	background-color:black;
-	font-family: 'Audiowide', cursive;
-	color:white;
-	font-size:16px;
-	
-  }
-  #hej1
-  {
- 
-  margin-left:35%;
-  margin-right:35%;
-  }
-  input[type=text],
-  [type=password]
-  {
-	 margin-top:30px;
-	 background-color:black;
-	 border:3px solid white;
-	 font-family: 'Audiowide', cursive;
-	border-radius:10px;
-	 margin-right:10px;
-	 width:33%;
-	 color:white;
-  }
-  
- input[type=submit]
-  {
-	 
-	border-radius:10px;
-	margin-top:15px;
-	border:2px solid white;
-	background-color:black;
-	color:white;
-	font-family: 'Audiowide', cursive;
-	cursor:pointer;
-
-  }
-  input:focus
-  {
-	  background-color:#424242;
-	  
-  }
-  #sub
-  {
-	  margin-left:35%;
-	  margin-right:47%;
-	  
-	  text-algin:center;
-  }
-  #uno
-  {
-	  
-	  width:500px;
-	  height:60px;
-	  
-  }
-  </style>
   <div id="hej1" >
   
   
