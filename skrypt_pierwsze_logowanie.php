0d36b8acc3f0374c9cf323270c7ba3766b29a23c
DawidPlocharski123 <34648643+DawidPlocharski123@users.noreply.github.com>
2018-02-05 12:44:41 +0100

    Naprawienie skryptu
    (Dawid i Asia)

--- skrypt_pierwsze_logowanie.php
+++ skrypt_pierwsze_logowanie.php
@@ -2,14 +2,31 @@
 session_start();  
 require "baza.php";
 $email=$_POST['email'];
+ $login=$_SESSION['login'];
+ $haslo =$_SESSION['haslo'];
+$result=$wynik->query("update `loginy` set email='$email' where login='$login'");
 
-$wynik->query("update `loginy` (`email`) values ('$email')");
+if ($result = $wynik->query("SELECT * FROM `loginy` WHERE login='$login' and haslo='$haslo'"))
+{	
+
+	$w=$result->fetch_assoc();
+	$t=$w['typ'];
+	if($t=="nieprzydzielony")
+	{
+	header ('Location:pierwsze_logowanie.php');
+	}
+	if($t=="uczen")
+	{
+	
+		header ('Location:IndexU/index1.php');
+			
+			  
+	}
+}
 
 
 
-$wynik->close();
-		
 
-header('Location: panel_dodania_ucznia.php');
+header('Location:IndexU/index1.php');
 //$wynik->query("INSERT INTO `lekcje` (`lekcja`) VALUES ('$kat')");
 ?>
