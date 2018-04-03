c1a57a340ae487472b5690be2b1791e383013fb7
1-9-9-8 <34984227+1-9-9-8@users.noreply.github.com>
2018-04-03 20:39:50 +0200

    uporządkowanie kodu

--- IndexA/panel_dodania_dyrektora.php
+++ IndexA/panel_dodania_dyrektora.php
@@ -2,92 +2,72 @@
 session_start();
  ?>
 <!DOCTYPE html>
-
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-<link rel="Stylesheet" type="text/css" href="style.css" />
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+	<link rel="Stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
-	margin:0%;
-	font-size:35px;
+	margin:0px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-  }
+}
 #okno
 {
-	
-	width:350px;
-	height:100px;
-	margin-left:9%;
-}
-#tekst{
-		margin-right:20%;
-}  
-  </style>
-
+	margin-left:5%;
+}   
+</style>
 <div id="all">
 <div id="logo">
 Panel Admina 
 <img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
+
 <form action="../wyloguj.php" method="_POST" >
- 
-
-  <input style="float:right;background-color:#191919;:width:50px;height:98px;position:relative;
+  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
     bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
-</div>
-</div>
+</form>
 
+</div>
+<form method="post" action="admin.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
+
+<div id="okno">
 <form action="skrypt_dodawania_dyrektora.php" method="POST" >
+	Wybierz nauczyciela, który ma zostać dyrektorem:  
     <select style="width:120px;height:30px;border-radius:400px;" name='nauc'>
     <?php
-require "baza.php";
- 
-if ($result = $wynik->query("SELECT * FROM loginy WHERE typ <> 'admin' and typ <> 'uczen'")) {
-   
-    while($w=$result->fetch_assoc()){
-        echo "<option value=".$w['id'].">".$w['login']."</option>";      
-        $_SESSION['login']=$w['login'];
-		
-       
+	require "baza.php";
+	if ($result = $wynik->query("SELECT * FROM loginy WHERE typ <> 'admin' and typ <> 'uczen'"))
+		{
+			while($w=$result->fetch_assoc())
+			{
+				echo "<option value=".$w['id'].">".$w['imie']." ".$w['nazwisko']."</option>";      
+			}
+			$result->close();
 	}
- 
-  $result->close();
- 
-}
-?>
- </select>
-  <input type="submit" value="Dodaj"/>
- </div>
+	?>
+	</select>
+	<br>
+	<input type="submit" value="Wybierz">
+</form>
 
- 
- 
- </form>
-  <input type='button' onclick="window.location.href='admin.php'" id="cofnijdopaneluadmina" value="Cofnij do panelu">
+</div>
 </body>
 </html>
--- IndexA/skrypt_dodawania_dyrektora.php
+++ IndexA/skrypt_dodawania_dyrektora.php
@@ -1,33 +1,9 @@
-
-<!DOCTYPE html>
-
-<html>
-<?php 
-session_start()
- ?>
-<head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-
-</head>
-<body>
- 
-
-
-   
- <?php
- require "baza.php";
+<?php
+require "baza.php";
   
 $nau=$_POST['nauc'];
-$wynik->query("INSERT INTO dyrektorzy (`nauczyciel`) VALUES ('$nau')");
-
+$wynik->query("INSERT INTO `dyrektorzy` (`nauczyciel`) VALUES ('$nau')");
 $wynik->close();
 header('Location: panel_dodania_dyrektora.php');		
 		
- ?>
- 
- 
-
-
-</body>
-</html>
+?>
