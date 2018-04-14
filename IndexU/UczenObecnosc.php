a78affe00ad17188cdb4129d6de49f34ba0f8fd5
1-9-9-8 <34984227+1-9-9-8@users.noreply.github.com>
2018-04-14 17:27:20 +0200

    poprawa estetyki kodu
    drobne poprawki

--- IndexU/UczenObecnosc.php
+++ IndexU/UczenObecnosc.php
@@ -1,160 +1,112 @@
 <?php 
 session_start();
-
 require "baza.php";
-
-
-
- ?>
+?>
 <!DOCTYPE html>
-
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	
-	font-size:35px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-	
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-	
-  }
+}
 #okno
 {
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
+	margin-left:5%;
 }
-  
-  </style>
+</style>
 
 <div id="all">
 <div id="logo">
-SEMESTR1
-<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
+Panel Ucznia
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
+</form>
 </div>
 
+<form method="post" action="index1.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
+
+<div id="okno">
 Semestr 1
 <form method="POST" action="indexs2.php">
+	<input  type="submit" name="seme" value="semestr2" >
+</form>
+<form method="POST" action="wiadomosci.php">
+	<input  type="submit" name="seme" value="wiadomosc" >
+</form>
+<form method="POST" action="UczenObecnosc.php">
+	<input  type="submit" name="seme" value="obecnosc" >
+</form>
+</br>
 
-  <input  type="submit" name="seme" value="semestr2" >
-  </form>
-  <form method="POST" action="wiadomosci.php">
-
-  <input  type="submit" name="seme" value="wiadomosc" >
-  </form>
-  <form method="POST" action="UczenObecnosc.php">
-
-  <input  type="submit" name="seme" value="obecnosc" >
-  </form>
-
- 
-
-<br>
- 
- 
- 
-
-<br>
-legenda</br> U Nieobecnosc Uspr </br>    S spoznienie     </br>        - Nieobecnosc nieuspr   </br>   0 obecny
+legenda</br>
+U Nieobecnosc Uspr </br>
+S spoznienie</br>
+- Nieobecnosc nieuspr</br>
+0 obecny
+<hr>
+<?php
+$login= $_SESSION['login'];
+$id=$_SESSION['uczenid'];
+if ($result = $wynik->query("SELECT klasy.numer,klasy.nazwa,loginy.id,loginy.imie,loginy.nazwisko FROM loginy,klasy where klasy.id=loginy.klasa and loginy.login='$login'"))
+{
+    $w=$result->fetch_assoc();
+		$id=$w['id'];
+		$numer=$w['numer'];
+		$nazwa=$w['nazwa'];
+		$imie=$w['imie'];
+		$nazwisko=$w['nazwisko'];
+	echo $imie." ".$nazwisko;
+	echo "<br>Klasa ".$numer." ".$nazwa."<br>";
+}
+?>
 <div style="background-color:;"id="panel">
 
- <div style="float:left;background-color:;width:20%;min-height:30px" > Login    </div>
-	  <div style="float:left;background-color:;width:10%;min-height:30px" >    Klasa  </div>
-	  <div style="float:left;background-color:;min-width:35%;min-height:30px" >  Data </div>
-	  <div style="float:left;background-color:;width:35%;min-height:30px" >   Obecność  </div>
-
-    <div style="cleat:both" id="clear"> </div>
+	<div style="float:left;width:45%;min-height:30px">Data</div>
+	<div style="float:left;width:45%;min-height:30px">Obecność</div>
+    <div style="cleat:both" id="clear"></div>
 <?php
-
-$login= $_SESSION['login'];
-
- 
- 
- if ($result = $wynik->query("SELECT klasy.klasa,loginy.id FROM loginy,klasy where klasy.id=loginy.klasa and loginy.login='uczenkl12'")) {
-   
-    while($w=$result->fetch_assoc()){
-		
-      $id= $w['id'];
-	  $klasa=$w['klasa'];
-	}
- }
-
-
-
-
- $i=0;
- if ($result = $wynik->query("SELECT * FROM `obeconsc` WHERE `uczen`=$id AND `obecnosc`=2 OR `obecnosc`=11 GROUP BY `data`")) {
-   
-    while($w=$result->fetch_assoc()){
-		
-      ?>
-	  
-	  <div style="float:left;background-color:;width:19%;min-height:30px;border:1px dotted black" id="login"> <?php echo $login  ?>     </div>
-	  <div style="float:left;background-color:;width:8%;min-height:30px;border:1px dotted black" id="klasa"> <?php  echo $klasa ?>     </div>
-	   <div style="float:left;background-color:;width:25%;min-height:30px;border:1px dotted black" id="przedmiot">   <?php echo $w['data']; ?>     </div>
-	  <div style="float:left;background-color;min-width:39%;min-height:40px;border:1px dotted black" id="Oceny">    <?php require "SkryptSprObecnosciUcznia.php"; ?>  </div>
-
-	
-	  <div style="cleat:both" id="clear">      </div>
-	 <?php 
-		
-	 
-	 
-	
-	 ?>
-	 <br>
-	 
-	 <?php 
-		
-       
-    }
- }
-
+$i=0;
+if ($result = $wynik->query("SELECT * FROM `obeconsc` WHERE `uczen`=$id AND `obecnosc`=2 OR `obecnosc`=11 GROUP BY `data`"))
+{
+    while($w=$result->fetch_assoc())
+	{
 ?>
+<div style="float:left;width:45%;min-height:30px;border:1px dotted black" id="przedmiot"><?php echo $w['data']; ?></div>
+<div style="float:left;width:45%;min-height:40px;border:1px dotted black" id="Oceny"><?php require "SkryptSprObecnosciUcznia.php"; ?></div>
 
+<div style="cleat:both" id="clear"></div>
+
+<br>
+<?php 
+}
+}
+?>
 </div>
-
-
-
-   
-
- </div>
-    
- </div>
+</div>
+</div>
 </body>
 </html>
--- IndexU/index1.php
+++ IndexU/index1.php
@@ -2,394 +2,294 @@
 session_start();
  ?>
 <!DOCTYPE html>
-
 <html>
-
 <head>
-	
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
- <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	font-size:25px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
-  }
-  
-  </style>
+	text-align: center;
+	padding:9px;
+	color:white;
+}
+#okno
+{
+	margin-left:5%;
+	width: 80%;
+	float: left;
+}
+</style>
 
 <div id="all">
 <div id="logo">
+Panel Ucznia
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-<img style="height:125px;width:200px;margin-top:-20px;"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
-
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 109px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
+</form>
 </div>
-<div style="float:left;width:85%" id="content">
 
+<div id="okno">
+
+<form method="POST" action="indexs2.php">
+	<input type="submit" name="seme" value="Semestr 2">
+</form>
+<form method="POST" action="wiadomosci.php">
+	<input  type="submit" name="seme" value="Wyślij wiadomość">
+</form>
+<form method="POST" action="UczenObecnosc.php">
+	<input  type="submit" name="seme" value="Twoje obecności">
+</form>
+<form method="POST" action="haslo1.php">
+	<input  type="submit" name="zmiana hasła" value="Zmiana hasła" >
+</form>
+<br>
 
 Semestr 1
-<form method="POST" action="indexs2.php">
-
-  <input  type="submit" name="seme" value="semestr2" >
-  </form>
-  <form method="POST" action="wiadomosci.php">
-
-  <input  type="submit" name="seme" value="wiadomosc" >
-  </form>
-  <form method="POST" action="UczenObecnosc.php">
-
-  <input  type="submit" name="seme" value="obecnosc" >
-  </form>
-  <form method="POST" action="haslo1.php">
-  <input  type="submit" name="zmiana hasła" value="Zmiana hasła" >
-  </form>
- <?php  
-$login= $_SESSION['login'];
+<?php  
+$login=$_SESSION['login'];
 require "baza.php";
-//if(!isset($_SESSION['zaloguj']))
-//{
-	//header ('Location:../login.php');
-	//exit();
-}
-
-if ($result89 = $wynik->query("SELECT * FROM `loginy` where login='$login'")) {
-   
-    while($s=$result89->fetch_assoc()){
-      
-       $_SESSION['uczenid']=$s['id'];
-      $uczenid=$s['id'];
+if ($result89 = $wynik->query("SELECT * FROM `loginy` where login='$login'"))
+{
+	while($s=$result89->fetch_assoc())
+	{
+		$_SESSION['uczenid']=$s['id'];
+		$_SESSION['klasa']=$s['Klasa'];
+		$uczenid=$s['id'];
+		$_SESSION['uczenid']=$uczenid;
     }
-	
-	
 }
 echo  $uczenid;
 
 $result69 = $wynik->query("select typ from loginy where login='$login' and typ='admin'");
-	if($result69->num_rows==1 )
-	{
-	   
-	   $_SESSION['admin']=true;
-	   header ('Location:../IndexA/admin.php');
-	   
-	   
-		 
-		
-	}
-	$result79 = $wynik->query("select typ from loginy where login='$login' and typ='teacher'");
-	if($result79->num_rows==1 )
-	{
-	   
-	   $_SESSION['nau']=true;
-	   header ('Location:../IndexN/panel_wyboru_lekcji_do_dodania_Ocen.php');
-	   
-	   
-	}
-	
-	 $result69->close();
-	  $result79->close();
-		  
-
-
-
- 
+if($result69->num_rows==1 )
+{ 
+	$_SESSION['admin']=true;
+	header ('Location:../IndexA/admin.php');	
+}
+$result79 = $wynik->query("select typ from loginy where login='$login' and typ='teacher'");
+if($result79->num_rows==1 )
+{ 
+	$_SESSION['nau']=true;
+	header ('Location:../IndexN/panel_wyboru_lekcji_do_dodania_Ocen.php');
+}
+$result69->close();
+$result79->close();
 echo $login;
-
-
-
-
-
-
-
-
-
 
 $poms=0;
 $sua=0;
  $id=0;
-if ($result1 = $wynik->query("SELECT klasa FROM loginy WHERE login='$login'")) {
-   
-    while($w=$result1->fetch_assoc()){
-			 $id=$w['klasa'];
-			 $idklasa=$id;
-      
+if ($result1 = $wynik->query("SELECT klasa FROM loginy WHERE login='$login'"))
+{
+    while($w=$result1->fetch_assoc())
+	{
+		$id=$w['klasa'];
+		$idklasa=$id;
     }
-	}
-if ($result11 = $wynik->query("SELECT oceny.oceny from loginy,oceny where loginy.id=oceny.kl and loginy.Klasa='$id' ")) {
-   
-    while($s=$result11->fetch_assoc()){
-      $sua=$sua + $s['oceny'];
-       $poms++;
-      
+}
+if ($result11 = $wynik->query("SELECT oceny.oceny from loginy,oceny where loginy.id=oceny.kl and loginy.Klasa='$id' "))
+{  
+    while($s=$result11->fetch_assoc())
+	{
+		$sua=$sua + $s['oceny'];
+		$poms++;
     }
 	@$sua=$sua/$poms;
-	
 }
- $sua=round($sua,2); 
-
-
-
- ?></h1>
- 
-
-
+$sua=round($sua,2); 
+?>
 </br>
+<?php
+if ($result1 = $wynik->query("SELECT * FROM `lekcje` WHERE id!=0"))
+{
+    while($w=$result1->fetch_assoc())
+	{ 
+		$lekcja= $w['lekcja'];
+		$lekcjaid=$w['id'];
+?>
+Oceny z <?php  echo $lekcja;?>
+</br> 
+<ol style=" list-style-type:none;padding:0;margin:0;">
+<?php 
+if ($result131 = $wynik->query("SELECT * FROM `oceny`,loginy where idlogin='$uczenid' and idlekcja='$lekcjaid' and `oceny`.idnauczyciel=loginy.id"))
+{
+    while($s=$result131->fetch_assoc())
+	{
+?>
+	<li id="c<?php echo $s['id'] ?>"style="background-color:
+		<?php 
+			if($s['oceny']==1)echo "#D50000";
+			if($s['oceny']==2)echo "#E53935";
+			if($s['oceny']==3)echo "#FFEB3B";
+			if($s['oceny']==4)echo "#388E3C";
+			if($s['oceny']==5)echo "#1B5E20";
+			if($s['oceny']==6)echo "#01579B";
+		?>
+		;text-align:center; display:block;width:50px;height:50px;float:left;font-size:40px;border:2px white solid;border-radius:10px;"><?php  echo $s['oceny'] ;   ?>
+		<ul style=" list-style-type:none;padding:0;margin:0;">
+			<li id="d<?php echo $s['id'] ?>" style="width:200px;height:90px;background-color:#BDBDBD;z-index:1;font-size:19px;line-height:1.5em;border:2px white solid;border-radius:10px;text-align: center;margin-top:5px;">
+				<?php  echo "Zaco "; echo '"';echo $s['zaco'] ;echo '"'; echo "</br>" ;
+				echo "Nauczyciel "; echo '"';echo $s['login'] ;echo '"'; echo "</br>" ;
+				echo "Data "; echo '"';echo $s['Data'] ;echo '"'; echo "</br>" ;   ?>
+			</li>
+		</ul>
+	</li>
+	<?php echo " " ;?>
+	<style>
+	#c<?php echo $s['id'] ?>:hover
+	{
+		color:red;
+	}
+	ol > li > ul
+	{
+		display:none;
+	}
+	ol > li > ul>li
+	{
+		color:blue;
+		z-index: 1;
+		position:relative; 
+	}
+	ol > li:hover > ul
+	{
+		display:block;
+	}
+	</style>
+	<?php } ?>
+</ol>
 
-
-
-
+<div style="clear:both"></div>
 
 <?php
-
-
-if ($result1 = $wynik->query("SELECT * FROM `lekcje` WHERE id!=0")) {
-   
-    while($w=$result1->fetch_assoc()){
-			 
-			$lekcja= $w['lekcja'];
-			$lekcjaid=$w['id'];
-			 
-			 
-			 
-	?>		  Oceny z <?php  echo $lekcja;?>
- </br>
- 
-  
- <ol style=" list-style-type:none;padding:0;
-  margin:0;">
-  
- <?php
-
- 
- 
- 
-if ($result131 = $wynik->query("SELECT * FROM `oceny`,loginy where idlogin='$uczenid' and idlekcja='$lekcjaid' and `oceny`.idnauczyciel=loginy.id")) {
-  
-    while($s=$result131->fetch_assoc()){
-		?>
-		<li id="c<?php echo $s['id'] ?>"style="background-color:<?php 
-					if($s['oceny']==1)echo "#D50000";
-					if($s['oceny']==2)echo "#E53935";
-					if($s['oceny']==3)echo "#FFEB3B";
-					if($s['oceny']==4)echo "#388E3C";
-					if($s['oceny']==5)echo "#1B5E20";
-					if($s['oceny']==6)echo "#01579B";
-		?>;text-align:center; display:block;width:50px;height:50px;float:left;font-size:40px;border:2px white solid;border-radius:10px;"><?php  echo $s['oceny'] ;   ?>
-		
-			<ul style=" list-style-type:none;padding:0;
-  margin:0;">
-		<li id="d<?php echo $s['id'] ?>" style="width:200px;height:90px;background-color:#BDBDBD;z-index:1;font-size:19px;line-height:1.5em;border:2px white solid;border-radius:10px;text-align: center;margin-top:5px;">
-		
-		<?php  echo "Zaco "; echo '"';echo $s['zaco'] ;echo '"'; echo "</br>" ;
-
-		echo "Nauczyciel "; echo '"';echo $s['login'] ;echo '"'; echo "</br>" ;
-		echo "Data "; echo '"';echo $s['Data'] ;echo '"'; echo "</br>" ;   ?>
-		
-		 
-		</li>
-		
-		
-		</ul>
-		</li> <?php
-           
-		  echo " " ;    
-       ?>
-	   <style>
-	   #c<?php echo $s['id'] ?>:hover
-	   {
-		   color:red;
-		   
-		  
-	   }
-
-	   ol > li > ul {
-  display:none;
-}
-   ol > li > ul>li {
-  color:blue;
-   z-index: 1;
-   position:relative;
-   
-}
-
-ol > li:hover > ul {
-  display:block;
-}
-	   </style>
-	   <?php
-	   
-	   
-      
-    }
-?></ol>
- <div style="clear:both"></div>
- <?php
-  $result131->close();
-  
+$result131->close();  
 }
 }}
- 
- 
 ?>
 
-  
-		</div>
-		<div style="float:left:width:50%" id="uwagi" >
-		
-		
-		
-		
-		uwagi
-		<?php
-			 if ($result1 = $wynik->query("SELECT * FROM loginy WHERE login='$login'")) {
-   
-    while($w=$result1->fetch_assoc()){
-			 $iduwagi=$w['id'];
-      
-    }
-	}
-	
-		 if ($result1 = $wynik->query("SELECT * FROM uwagi WHERE id_login='$iduwagi'")) {
-   
-    while($w=$result1->fetch_assoc()){
-			 
-			 ?>
-			 <div style="margin-left:84%; background-color:#9E9E9E" class="uwagii" > Tytul <?php
-      
-				
-				echo $w['tytul'];
-				?>
-				</br>
-	
-	 <div style="padding-left:17%;; background-color:#616161" class="uwagii" >  Tresc </br><?php
-      
-				
-				echo $w['tresc'];
-				?>
-				</br>
-	 
-	 
-			 
-			 
-			 </div>
-			 
-			 
-			 </div>
-			 
-			 <?php
-      
-	  
-	  
-    }
-	}
-		?>
-		
-		tu bendom wywiadowek
-		<?php
-  if ($result69 = $wynik->query("SELECT * FROM loginy WHERE login='$login'")) {
-			
-				 
-    while($w=$result69->fetch_assoc()){
-			 $klasa=$w['Klasa'];
-      
-    }
-  }
-			 if ($result1 = $wynik->query("SELECT * FROM wywiadowka WHERE klasa='$klasa'")) {
-				
-				 
-    while($w=$result1->fetch_assoc()){
-			 $wywiadowka=$w['klasa'];
-    }
-	}
-	
-		 if ($result1 = $wynik->query("SELECT * FROM wywiadowka WHERE $wywiadowka='$klasa'")) {
-   if ($result1 = $wynik->query("SELECT * FROM wywiadowka WHERE aktywna='1'"))
-   {
-    while($w=$result1->fetch_assoc()){
-			 
-			 ?>
-			 <div style="margin-left:84%; background-color:#9E9E9E" class="uwagii" ><?php
-      
-				
-				echo $w['data'];
-				echo " w sali ";
-				echo $w['sala'];
-				?>
-				</br>
-	
-	 <div style="padding-left:17%;; background-color:#616161" class="uwagii" ><?php
-      
-				
-				echo $w['tytul'];
-				?>
-				</br>
-	 
-	 
-			 
-			 
-			 </div>
-			 
-			 
-			 </div>
-			 
-			 <?php
-      
-	  
-	}
-    }
-	}
-	echo $idklasa;
-		?>
-		
-		</div>
-		<div id="diagrams" style="width:700px;height:900px; ">
-		
-		
-		<?php
-		$xd="dailydiagrams/diagram11.php";
-		 
-		
+</div>
 
-		$xd[21]=rand(0,0);
-		$xd[22]=rand(0,3);
-		echo $xd;
-		?>
-		</br>
-		<span style="Margin-left:180px;font-size:35px;">Refresh  Diagram Beta</span>
-		<?php
-		 require  $xd;
-		?>
+<div style="float:left:width:50%" id="uwagi">
+uwagi
+<?php
+if ($result1 = $wynik->query("SELECT * FROM loginy WHERE login='$login'"))
+{
+	while($w=$result1->fetch_assoc())
+	{
+		$iduwagi=$w['id'];
+    }
+}
+if ($result1 = $wynik->query("SELECT * FROM uwagi WHERE id_login='$iduwagi'")) 
+{
+	while($w=$result1->fetch_assoc())
+	{
+?>
+
+<div style="margin-left:84%; background-color:#9E9E9E" class="uwagii" >Tytul 
+<?php
+echo $w['tytul'];
+?>
+</br>
+	
+<div style="padding-left:17%;; background-color:#616161" class="uwagii" >Tresc</br>
+<?php
+echo $w['tresc'];
+?>
+</br>	 
+</div>
+			 
+</div>
+			 
+<?php
+}
+}
+?>
 		
+Informacje o wywiadówkach
+<?php
+if ($result69 = $wynik->query("SELECT * FROM loginy WHERE login='$login'"))
+{			 
+    while($w=$result69->fetch_assoc())
+	{
+		$klasa=$w['Klasa'];
+    }
+}
+if ($result1 = $wynik->query("SELECT * FROM wywiadowka WHERE klasa='$klasa'"))
+{			 
+    while($w=$result1->fetch_assoc())
+	{
+		$wywiadowka=$w['klasa'];
+    }
+}
+if ($result1 = $wynik->query("SELECT * FROM wywiadowka WHERE $wywiadowka='$klasa'")) 
+{
+	if ($result1 = $wynik->query("SELECT * FROM wywiadowka WHERE aktywna='1'"))
+	{
+		while($w=$result1->fetch_assoc())
+		{
+?>
+
+<div style="margin-left:84%; background-color:#9E9E9E" class="uwagii">
+<?php
+echo $w['data'];
+echo " w sali ";
+echo $w['sala'];
+?>
+</br>
+	
+<div style="padding-left:17%;; background-color:#616161" class="uwagii" >
+<?php
+echo $w['tytul'];
+?>
+</br>
+</div>
+			 
+</div>
+			 
+<?php
+}
+}
+}
+echo $idklasa;
+?>
 		
-		</div>
+</div>
 		
-		</div> 
+<div id="diagrams" style="width:700px;height:900px;">
+<?php
+$xd="dailydiagrams/diagram11.php";
+$xd[21]=rand(0,0);
+$xd[22]=rand(0,3);
+echo $xd;
+?>
+<br>
+
+<span style="Margin-left:180px;font-size:35px;">Refresh  Diagram Beta</span>
+<?php
+require  $xd;
+?>
+</div>
+		
+</div> 
 </body>
 </html>
