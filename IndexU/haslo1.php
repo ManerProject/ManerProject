59065f7384f0dabc89c0969ee6b709f48483926e
1-9-9-8 <34984227+1-9-9-8@users.noreply.github.com>
2018-04-14 14:42:12 +0200

    poprawienie wyglądu strony (estetyka kodu)

--- IndexU/haslo1.php
+++ IndexU/haslo1.php
@@ -2,82 +2,69 @@
 session_start();
 require "baza.php";
 $data= date('Y.m.d');
-
 for($j=0;$j<strlen($data);$j++)
 {
 	if($data[$j]=='.')
+	{
 		$data[$j]='-';
-	
+	}		
 }
-
-
- ?>
+?>
 <!DOCTYPE html>
-
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	
-	font-size:35px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-	
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-	
-  }
+}
 #okno
 {
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
+	margin-left:5%;
 }
-  
-  </style>
+</style>
 
 <div id="all">
 <div id="logo">
-<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
+Panel Ucznia
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
+</form>
 </div>
-<form method="post" action="skrypthaslo1.php" style="font-size:15px;">
-<input type="password" name="haslo" style="width:100px; height:25px;border-radius:4px;" placeholder="podaj stare hasło"value="">
-<input type="password" name="nhaslo" style="width:100px; height:25px;border-radius:4px;" placeholder="podaj nowe hasło"value="">
-<input type='submit' style="width:100px; height:50px;border-radius:400px;"value="ok ">
 
-    </form>
+<form method="post" action="index1.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
 
+<div id="okno">
+<form method="post" action="skrypthaslo1.php">
+	<input type="password" name="haslo" style="height:25px;border-radius:4px;" placeholder="Podaj stare hasło" value="">
+	<input type="password" name="nhaslo" style="height:25px;border-radius:4px;" placeholder="Podaj nowe hasło" value="">
+	<input type='submit' style="height:50px;border-radius:400px;"value="Zmień hasło">
+</form>
+
+</div>
+</div>
 </body>
 </html>
--- IndexU/skrypthaslo1.php
+++ IndexU/skrypthaslo1.php
@@ -1,18 +1,12 @@
 <?php 
 session_start();
- ?>
-<?php
- 
 require "baza.php";
  
- $haslo=$_POST['haslo'];
- $nhaslo=$_POST['nhaslo'];
+$haslo=$_POST['haslo'];
+$nhaslo=$_POST['nhaslo'];
 $nau=$_SESSION['login'];
 
- $wynik-> query("UPDATE `loginy` SET haslo='$nhaslo' WHERE haslo='$haslo' AND login='$nau'");
+$wynik-> query("UPDATE `loginy` SET haslo='$nhaslo' WHERE haslo='$haslo' AND login='$nau'");
  
-  
-  
-  header ("Location:index1.php");
-
+header ("Location:haslo1.php");
 ?>
