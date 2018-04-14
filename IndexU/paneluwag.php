a02cc0a88caf6abba512377e63a46e3fc813e13d
1-9-9-8 <34984227+1-9-9-8@users.noreply.github.com>
2018-04-14 14:45:18 +0200

    poprawa estetyki kodu

--- IndexU/paneluwag.php
+++ IndexU/paneluwag.php
@@ -2,113 +2,100 @@
 session_start();
  ?>
 <!DOCTYPE html>
-
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	
-	font-size:35px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-	
-  }
+}
 #okno
 {
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
-}
-  
-  </style>
+	margin-left:5%;
+}  
+</style>
 
 <div id="all">
 <div id="logo">
 Uwagi
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
-
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
+</form>
 </div>
 
- 
- </div>
- <div id="con">
-   <form method="post" action="skryptuwag.php" style="font-size:15px;">
-    <select style="width:120px;height:30px;border-radius:400px;" name='sele'>
-<?php
-require "baza.php";
-
-$klasa=$_SESSION['kl'];
-
-// test
-// test2
-	
-echo $klasa; 
-	    if ($result = $wynik->query("SELECT * FROM loginy WHERE typ <> 'admin' and typ <> 'teacher' and klasa='$klasa'")) {
-   
-    while($w=$result->fetch_assoc()){
-        echo "<option value=".$w['login'].">".$w['login']."</option>";      
-      
-		
-       
-    }
- 
- 
-  $result->close();
-  $wynik->close();
-}
-		 
-		  
-	
-	
-?>
-
-    </select>
-	
-   <input name="tytul" style="width:90px; height:25px;border-radius:4px;" placeholder="tytuł"value="">
-	<input name="tresc"style="width:200px; height:150px;border-radius:400px;" placeholder="tresc"value="">
-	
-	<input type='submit' style="width:100px; height:50px;border-radius:400px;"value="ok ">
-
+<form method="post" action="index1.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
+<div id="okno">
+	<form method="post" action="skryptuwag.php" style="font-size:15px;">
+		<select style="width:120px;height:30px;border-radius:400px;" name='sele'>
+		<?php
+		require "baza.php";
+		//$klasa=$_SESSION['klasa'];
+		$klasa=9;
+		if ($result = $wynik->query("SELECT * FROM `nau` WHERE `idklasa`='$klasa'"))
+		{
+			$l=0;
+			$k=0;
+			while($w=$result->fetch_assoc())
+			{
+				$id=$w['nau'];
+				for($i=0;$i<$l;$i++)
+				{
+					if($id==$tab[$i])
+					{
+						$k=1;
+						break;
+					}
+				}
+				if($k==0)
+				{
+					$tab[$l]=$id;
+					$l++;
+				}
+				else if($k==1)
+				{
+					$k=0;
+					continue;
+				}
+				if($rez=$wynik->query("SELECT * FROM `loginy` WHERE id='$id' AND typ='teacher'"))
+				{
+					while($s=$rez->fetch_assoc())
+					{
+						echo "<option value=".$s['login'].">".$s['imie']." ".$s['nazwisko']."</option>";      
+					}
+				}
+			}
+		}	
+		?>
+		</select>
+		<input name="tytul" style="height:25px;border-radius:4px;" placeholder="tytuł wiadomości" value="">
+		<input name="tresc" style="height:25px;border-radius:4px;" placeholder="tresc" value="">
+		<input type='submit' style="height:50px;border-radius:400px;" value="Wyślij">
     </form>
-
 </br>
-
-
-
+</div>
 </div>
  
 </body>
