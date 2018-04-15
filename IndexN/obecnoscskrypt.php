4fd1154247b1f35bb6ee219883e29c7a64a35b0c
1-9-9-8 <34984227+1-9-9-8@users.noreply.github.com>
2018-04-15 17:57:44 +0200

    poprawa estetyki kodu
    drobne poprawki

--- IndexN/obecnoscskrypt.php
+++ IndexN/obecnoscskrypt.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
 require "baza.php";
-
-
-
-
 $data=$_SESSION['data'];
 $nl=$_POST['numer_lekcji'];
-
-
 $kurde=0;
 $kl= $_SESSION['kl'];
 
@@ -23,75 +17,53 @@ if($result=$wynik->query("SELECT * FROM `obeconsc` WHERE data='$data' and numer_
 		$kurde=0;
 	}
 }
-
 $login= $_SESSION['loginy'];
 
 for($j=0;$j<strlen($data);$j++)
 {
 	if($data[$j]=='.')
+	{
 		$data[$j]='-';
-	
+	}
 }
 $i=0;
 $xx=date('N');
-if ($result = $wynik->query("SELECT * FROM `planlekcji` WHERE klasa='$kl' and dzien='$xx'")) {
-   
-    while($w=$result->fetch_assoc()){
+if ($result = $wynik->query("SELECT * FROM `planlekcji` WHERE klasa='$kl' and dzien='$xx'"))
+{
+    while($w=$result->fetch_assoc())
+	{
 		$xd=$w['id'];
-		
-		
 	}
-	
 }
-
-     
-   if ($result = $wynik->query("SELECT * FROM `loginy` WHERE klasa='$kl'")) {
-   
-    while($w=$result->fetch_assoc()){
-     
-	 
-	
-	 
-	 
-	 if($_POST['obecnosc'][$i]=="1 Obecny")
-		 $obecnosc=1;
-	 if($_POST['obecnosc'][$i]=="2 Nie obecny")
-		 $obecnosc=2;
-	  if($_POST['obecnosc'][$i]=="3 Spoznienie")
-		 $obecnosc=3;
-	 $uczen=$w['id'];
-	
-
-if($kurde==1)
- {
-	 $wynik->query("UPDATE `obeconsc` SET obecnosc='$obecnosc' WHERE data='$data' and numer_lekcji='$nl' and klasa='$kl' and uczen='$uczen'");
-	 $i++;
-	 header('location:obecnosc.php');
- }
- else
- {
-	 $wynik->query("INSERT INTO `obeconsc` (`id`, `data`, `obecnosc`, `numer_lekcji`,`uczen`,`klasa`,`dzienTygodnia`) VALUES (NULL, '$data', '$obecnosc', '$nl', '$uczen','$kl','$xd')");
-	$i++;
-	header('location:obecnosc.php');
- }
-	 	 
-	 
-
-
-	 }
-   }
-  
- 
- 
-
-
-
-
-
-
-
-
-
-
-
+if ($result = $wynik->query("SELECT * FROM `loginy` WHERE klasa='$kl'")) 
+{
+    while($w=$result->fetch_assoc())
+	{
+		if($_POST['obecnosc'][$i]=="1 Obecny")
+		{
+		 	$obecnosc=1;
+		}
+		if($_POST['obecnosc'][$i]=="2 Nie obecny")
+		{
+			$obecnosc=2;
+		}
+		if($_POST['obecnosc'][$i]=="3 Spoznienie")
+		{
+			$obecnosc=3;
+		}
+		$uczen=$w['id'];
+		if($kurde==1)
+		{
+			$wynik->query("UPDATE `obeconsc` SET obecnosc='$obecnosc' WHERE data='$data' and numer_lekcji='$nl' and klasa='$kl' and uczen='$uczen'");
+			$i++;
+			header('location:obecnosc.php');
+		}
+		else
+		{
+			$wynik->query("INSERT INTO `obeconsc` (`id`, `data`, `obecnosc`, `numer_lekcji`,`uczen`,`klasa`,`dzienTygodnia`) VALUES (NULL, '$data', '$obecnosc', '$nl', '$uczen','$kl','$xd')");
+			$i++;
+			header('location:obecnosc.php');
+		}
+	}
+}
 ?>
--- IndexN/obserwacja.php
+++ IndexN/obserwacja.php
@@ -2,114 +2,98 @@
 session_start();
  ?>
 <!DOCTYPE html>
-
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	
-	font-size:35px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-	
-  }
+}
 #okno
 {
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
+	margin-left:5%;
 }
-  
-  </style>
+</style>
 
 <div id="all">
 <div id="logo">
-Panel wicedyrektora
-<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
+Panel Nauczyciela
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
-</div>
-<form   method="post" action="panel_wyboru_lekcji_do_dodania_Ocen.php" style="font-size:25px;">
-<input type='submit' style="width:100px; height:30px;margin-left:15%"value="Powrot do glownego panelu">
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
 </form>
-
-
- 	<div style="margin-left:9%;font-size:25px;float:left;">Obserwacja w klasie</div>
-	 </br>
-<form   method="post" action="skrypt_obserwacja.php" style="font-size:25px;">
-klasa
-<select name="klasa">
-<?php
-require "baza.php";
-$id=$_SESSION['id'];
-$_SESSION['id']=$id;
-if ($result1 = $wynik->query("SELECT * FROM klasy")) {
-   $i=1;
-    while($w=$result1->fetch_assoc()){
-			 $num=$w['numer'];
-			 $naz=$w['nazwa'];
-			 $id=$w['id'];
-			 echo "<option value='$id'>$num";
-			 echo " ";
-			 echo "$naz</option>";
-			 $i++;
-    }
-	}
-	?>
-</select>
-<br>
-nr lekcji 
-<select name="nr_l">
-	<option value="1">1</option>
-	<option value="2">2</option>
-	<option value="3">3</option>
-	<option value="4">4</option>
-	<option value="5">5</option>
-	<option value="6">6</option>
-	<option value="7">7</option>
-	<option value="8">8</option>
-	<option value="9">9</option>
-</select>
-<br>
- Data  <input type="date" value=""  style =""name="data" > <br>
- <input type="text" name="opinia">
-<input type='submit' style="width:100px; height:30px;margin-left:15%"value="Wprowadź dane">
-
-</form>	
-
 </div>
-    
+
+<form method="post" action="panel_wyboru_lekcji_do_dodania_Ocen.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
+<div id="okno">
+Obserwacja w klasie
+</br>
+
+<form method="post" action="skrypt_obserwacja.php" style="font-size:25px;">
+	klasa
+	<select name="klasa">
+		<?php
+		require "baza.php";
+		$id=$_SESSION['id'];
+		$_SESSION['id']=$id;
+		if ($result1 = $wynik->query("SELECT * FROM klasy"))
+		{
+			$i=1;
+			while($w=$result1->fetch_assoc())
+			{
+				$num=$w['numer'];
+				$naz=$w['nazwa'];
+				$id=$w['id'];
+				echo "<option value='$id'>$num";
+				echo " ";
+				echo "$naz</option>";
+				$i++;
+			}
+		}
+		?>
+	</select>
+	<br>
+	nr lekcji 
+	<select name="nr_l">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+	</select>
+	<br>
+	Data <input type="date" value=""  style =""name="data">
+	<br>
+	<input type="text" name="opinia">
+	<input type='submit' style="width:100px; height:30px;margin-left:15%"value="Wprowadź dane">
+</form>	
+</div>
+</div>   
 </body>
 </html>
