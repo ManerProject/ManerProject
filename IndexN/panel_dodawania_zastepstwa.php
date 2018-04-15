4a85b06ea3b1f04eddaea42b56c146ab56ff0918
1-9-9-8 <34984227+1-9-9-8@users.noreply.github.com>
2018-04-15 19:01:07 +0200

    poprawa estetyki kodu
    drobne poprawki

--- IndexN/panel_dodawania_zastepstwa.php
+++ IndexN/panel_dodawania_zastepstwa.php
@@ -1,134 +1,91 @@
 <?php 
 session_start();
- ?>
-<!DOCTYPE html>
-<?php
 $data= date('Y.m.d');
 
 for ($j=0; $j<strlen($data); $j++)
 {
 	if ($data[$j]=='.')
+	{
 		$data[$j]='-';
+	}
 }
 ?>
+<!DOCTYPE html>
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	
-	font-size:35px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-	
-  }
+}
 #okno
 {
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
+	margin-left:5%;
 }
-  
-  </style>
+</style>
 
 <div id="all">
 <div id="logo">
-Kurde wywiadówka
+Panel Nauczyciela
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
-
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
+</form>
 </div>
 
- 
- </div>
- <div id="con">
-   <form method="post" action="panel_dodawania_zastepstwa2.php" style="font-size:15px;">
-    <select style="width:120px;height:30px;border-radius:400px;" name='sele'>
-<?php
-require "baza.php";
-
-$klasa=$_SESSION['kl'];
-
-
-	
-echo $klasa; 
-	    if ($result = $wynik->query("SELECT * FROM klasy")) {
-   
-    while($w=$result->fetch_assoc()){
-        echo "<option value=".$w['id'].">".$w['klasa']."</option>";      
-      
-		
-       
-    }
-	
- 
- 
-  $result->close();
-  $wynik->close();
-}
-		 
-		  
-	
-?>
-
-
+<form method="post" action="panel_wyboru_lekcji_do_dodania_Ocen.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
+<div id="okno">
+Dodaj zastępstwo
+<form method="post" action="panel_dodawania_zastepstwa2.php">
+	<select style="height:30px;border-radius:400px;" name='sele'>
+	<?php
+	require "baza.php";
+	$klasa=$_SESSION['kl'];	
+	echo $klasa; 
+	if ($result = $wynik->query("SELECT * FROM klasy"))
+	{
+		while($w=$result->fetch_assoc())
+		{
+			echo "<option value=".$w['id'].">".$w['numer']." ".$w['nazwa']."</option>";      
+		}
+		$result->close();
+		$wynik->close();
+	}	
+	?>
     </select>
-	<select style="width:120px;height:30px;border-radius:400px;" name='sele2'>
-
-
-        <option value="1">poniedziałek</option> 
+	<select style="height:30px;border-radius:400px;" name='sele2'>
+		<option value="1">poniedziałek</option> 
 		<option value="2">wtorek</option> 
 		<option value="3">środa</option> 
 		<option value="4">czwartek</option> 
 		<option value="5">piątek</option> 
-  
-		
-       
-  
-	
-</select>
-
-	<input type='submit' style="width:100px; height:50px;border-radius:400px;"value="ok ">
-
-    </form>
-
-</br>
-
-
+	</select>
+	<input type='submit' style="height:25px;border-radius:4px;"value="Dodaj zastępstwo">
+</form>
 
 </div>
- 
+</div>
 </body>
 </html>
--- IndexN/panel_dodawania_zastepstwa2.php
+++ IndexN/panel_dodawania_zastepstwa2.php
@@ -1,133 +1,98 @@
 <?php 
 session_start();
- ?>
-<!DOCTYPE html>
-<?php
 $data= date('Y.m.d');
 
 for ($j=0; $j<strlen($data); $j++)
 {
 	if ($data[$j]=='.')
+	{
 		$data[$j]='-';
+	}
 }
 $sele=$_POST['sele'];
 $sele2=$_POST['sele2'];
 ?>
+<!DOCTYPE html>
 <html>
-
 <head>
-<meta lang="pl"/>
-<meta charset="utf-8"/>
-<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
-
+	<meta lang="pl"/>
+	<meta charset="utf-8"/>
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 </head>
 <body>
-  <style>
-  body
-  {
+<style>
+body
+{
 	background-color:#E6E6E6;
 	margin:0px;
-	
-	font-size:35px;
+	font-size:20px;
 	font-family: 'Acme', sans-serif;
-  }
-  #all
-  {
-	  width:100%;
-	  height:100%;
-  }
- #logo
-  {
- 
+}
+#logo
+{
 	width:100%;
-	height:100px;
+	min-height:102px;
 	background-color:black;
 	float:right
 	text-align: center;
-	pading:10px;
+	padding:9px;
 	color:white;
-	
-  }
+}
 #okno
 {
-	width:100px;
-	
-	height:100px;
-	margin-left:15%;
+	margin-left:5%;
 }
-  
-  </style>
+</style>
 
 <div id="all">
 <div id="logo">
-Kurde wywiadówka
+Panel Nauczyciela
+<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png">
 
-<img style="height:125px;width:200px;margin-top:-20px;float:left"  src="../Grafika/logomm.png"> </img>
-<form action="../wyloguj.php" method="_POST" >
- 
-
-  <input style="float:right;background-color:#191919;:width:50px;height:100px;position:relative;
-    bottom: 40px;cursor:pointer;color:white;font-family: 'Audiowide', cursive;border-left:2px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0" type="submit" value="WYLOGUJ"/>
- 
- 
- </form>
+<form action="../wyloguj.php" method="POST">
+  <input style="float:right;background-color:#191919;width:80px;height:70px;position:relative;
+    bottom:40px;cursor:pointer;color:white;font-family:'Audiowide', cursive;border-left:0px dotted #1F1F1F;border-top:0;border-right:0;border-bottom:0; margin-right:1%;" type="submit" value="WYLOGUJ"/> 
+</form>
 </div>
 
- 
- </div>
- <div id="con">
-   <form method="post" action="skrypt_zastepstwo.php" style="font-size:15px;">
-    <select style="width:120px;height:30px;border-radius:400px;" name='sele'>
-<?php
-require "baza.php";
-$_SESSION['data']=$data;
-$_SESSION['sele2']=$sele;
+<form method="post" action="panel_wyboru_lekcji_do_dodania_Ocen.php">
+	<input type='submit' style="height:30px; margin:1%" value="Powrót do głównego panelu">
+</form>
+<div id="okno">
 
-$klasa=$_SESSION['kl'];
-
-
-	
-echo $klasa; 
-	    if ($result = $wynik->query("SELECT * FROM planlekcji WHERE klasa='$sele' AND dzien='$sele2'")) {
-   
-    while($w=$result->fetch_assoc()){
-        echo "<option value='1'>".$w['lekcja1']."</option>";  
-		echo "<option value='2'>".$w['lekcja2']."</option>"; 
-		echo "<option value='3'>".$w['lekcja3']."</option>"; 
-		echo "<option value='4'>".$w['lekcja4']."</option>"; 
-		echo "<option value='5'>".$w['lekcja5']."</option>"; 
-		echo "<option value='6'>".$w['lekcja6']."</option>"; 
-		echo "<option value='7'>".$w['lekcja7']."</option>"; 
-		echo "<option value='8'>".$w['lekcja8']."</option>"; 
-		echo "<option value='9'>".$w['lekcja9']."</option>"; 		
-      
-		
-       
-    }
- 
- 
-  $result->close();
-  $wynik->close();
-}
-		 
-		  
-	
-	
-?>
-<input type="text" placeholder="lekcja" name="lekcja"/>
-<input type="number" placeholder="Numer sali" name="numer_sali"/>
-
+<form method="post" action="skrypt_zastepstwo.php">
+    <select style="height:30px;border-radius:400px;" name='sele'>
+	<?php
+	require "baza.php";
+	$_SESSION['data']=$data;
+	$_SESSION['sele2']=$sele;
+	$klasa=$_SESSION['kl'];
+	echo $klasa; 
+	if ($result = $wynik->query("SELECT * FROM planlekcji WHERE klasa='$sele' AND dzien='$sele2'")) 
+	{
+		while($w=$result->fetch_assoc())
+		{
+			echo "<option value='1'>".$w['lekcja1']."</option>";  
+			echo "<option value='2'>".$w['lekcja2']."</option>"; 
+			echo "<option value='3'>".$w['lekcja3']."</option>"; 
+			echo "<option value='4'>".$w['lekcja4']."</option>"; 
+			echo "<option value='5'>".$w['lekcja5']."</option>"; 
+			echo "<option value='6'>".$w['lekcja6']."</option>"; 
+			echo "<option value='7'>".$w['lekcja7']."</option>"; 
+			echo "<option value='8'>".$w['lekcja8']."</option>"; 
+			echo "<option value='9'>".$w['lekcja9']."</option>"; 		  
+		}
+		$result->close();
+		$wynik->close();
+	}	
+	?>
+	<input type="text" placeholder="lekcja" name="lekcja"/>
+	<input type="number" placeholder="Numer sali" name="numer_sali"/>
     </select>
-
-	<input type='submit' style="width:100px; height:50px;border-radius:400px;"value="ok ">
-
-    </form>
-
-</br>
-
-
+	<input type='submit' style="height:25px;border-radius:4px;"value="Dodaj zastępstwo">
+</form>
 
 </div>
- 
+</div>
 </body>
 </html>
